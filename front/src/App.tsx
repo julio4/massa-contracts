e3f466fc2845d22296bfe56dc234a048d592a251
Julio4 <jules.doumeche@gmail.com>
2023-03-11 11:04:29 +0100

    initial commit

---
+++ front/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { Dapp } from './Dapp';
+
+const App: React.FC = () => {
+    return (
+        <div className="App">
+          <Dapp />
+        </div>
+    );
+};
+
+export default App;
